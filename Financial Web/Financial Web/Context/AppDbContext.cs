125250259f2ffdcd87ab5455f8da75639730888e
Pedro Ferrette <pedrovf@live.com>
2022-05-03 16:34:35 -0300

    Adição da classe Startup e dos Pacotes Entity

---
+++ Financial Web/Financial Web/Context/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Financial_Web.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions <AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
