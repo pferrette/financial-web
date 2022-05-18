7ec565ffbede85974ae44ea7ca31b429c743af03
Pedro Ferrette <pedrovf@live.com>
2022-05-17 23:22:42 -0300

    Testando ideias para o front end de Gastos

---
+++ Financial Web/Financial Web/Controllers/GastosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Financial_Web.Controllers
+{
+    public class GastosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
